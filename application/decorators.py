278d0bce697279508e9893f229403385cedab069
Martin Spier <spiermar@gmail.com>
2013-09-29 15:41:39 -0700

    Fixing 401 response.

--- application/decorators.py
+++ application/decorators.py
@@ -104,10 +104,7 @@ def check_auth(username, password):
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
-    return Response(
-        'Could not verify your access level for that URL.\n'
-        'You have to login with proper credentials', 401,
-        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    abort(401)
 
 
 def requires_auth(f):
